096ddaee6950d9763fc93729817cb3ebe1368ffb
Daniel Sepulveda <daniel-thorney@hotmail.com>
2022-10-20 01:55:24 -0300

    Modificacion de index
    Se implementa el index de la pagina, utilizando la documentacion de Flask y se conecta a la base de datos (sin exito momentaneo). Cambios menores de sintaxis en style.css y layout.html
    
    Co-Authored-By: Leonardo Acum <114183959+Chocmanblack@users.noreply.github.com>
    Co-Authored-By: Hernan Barrales <114186271+Crueltarsier@users.noreply.github.com>

--- mywebsite/index.py
+++ mywebsite/index.py
@@ -1,14 +1,66 @@
-from flask import Flask, render_template
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/") #decorador
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/veterinaria.db'
+
+# db = SQLAlchemy(app)
+
+def get_db_connection():
+    conn = sqlite3.connect('instance/database.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/home") #decorador
 def index():
     return render_template("index.html")
 
-@app.route("/login") #decorador
+@app.route("/", methods=['GET', 'POST']) #decorador
 def login():
-    return render_template("login.html")
+    metodo = request.method
+    if(metodo == 'GET'):
+        return render_template("login.html")
+    elif (metodo == 'POST'):
+        #coneccion 
+        conn = get_db_connection();
+        #login de ingreso
+
+        #captura de datos
+        rut = request.form['rut']
+        pass_ = request.form['pass']
+
+        if not rut:
+            error = 'El rut es requerido.'
+        elif not pass_:
+            error = 'La contraseña es requerida'
+        else:
+            user = conn.execute(
+                'SELECT * FROM user WHERE rut = ?', (rut,)
+            ).fetchone()
+
+            if user is None:
+                error = 'Usuario incorrecto'
+            elif user["pass"] != pass_:
+                error = 'Contraseña incorrecta.'
+
+            if error is None:
+                session.clear()
+                session["user_id"] = user['id']
+                return redirect(url_for('home'))
+            else:
+                flash(error)
+
+        return render_template("index.html")
+    else:
+        #404
+        return render_template("404.html")
+
+
+
+
 
 @app.route("/register") #decorador
 def register():
