5002b6ef23b6679696d1b986c9504958924a19c7
Daniel Sepulveda <daniel-thorney@hotmail.com>
2022-11-02 21:15:01 -0300

    corrección error 404
    pequeña corrección realizada que evitaba la renderización del error 404 al no encontrar la pagina

--- mywebsite/index.py
+++ mywebsite/index.py
@@ -104,7 +104,7 @@ def about():
     return render_template("about.html")
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
